fd4e7189b8412f6179be88a91c357f54968e3cf3
Kunal-inno <kunalgupta@innotechteam.in>
2023-01-12 10:42:56 +0530

    make branch counter3

--- src/CounterDisplay.js
+++ src/CounterDisplay.js
@@ -1,21 +1,56 @@
+// import React, { useEffect, useState } from "react";
+// import "./CounterDisplay.css";
+
+// const CounterDisplay = () => {
+//   const [counter, setCounter] = useState(0);
+
+//   useEffect(() => {
+//     const id = setInterval(() => setCounter((oldCount) => oldCount + 1), 1000);
+
+//     return () => {
+//       clearInterval(id);
+//     };
+//   }, []);
+//   return (
+//     <div>
+//       <div className="card-parent">
+//         <p className="start-stop">Counter</p>
+
+//         <p className="sec">{counter}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CounterDisplay;
 import React, { useEffect, useState } from "react";
 import "./CounterDisplay.css";
 
 const CounterDisplay = () => {
   const [counter, setCounter] = useState(0);
+  const [isCounterRunning, setIsCounterRunning] = useState(true);
+  const [intervalId, setIntervalId] = useState(null);
 
   useEffect(() => {
-    const id = setInterval(() => setCounter((oldCount) => oldCount + 1), 1000);
+    if (isCounterRunning) {
+      setIntervalId(setInterval(() => setCounter((oldCount) => oldCount + 1), 1000));
+    } else {
+      clearInterval(intervalId);
+    }
 
     return () => {
-      clearInterval(id);
+      clearInterval(intervalId);
     };
-  }, []);
+  }, [isCounterRunning, intervalId]);
+
+  const handleCounterClick = () => {
+    setIsCounterRunning(!isCounterRunning);
+  };
+
   return (
     <div>
       <div className="card-parent">
-        <p className="start-stop">Counter</p>
-
+        <p className="start-stop" onClick={handleCounterClick}>Counter</p>
         <p className="sec">{counter}</p>
       </div>
     </div>

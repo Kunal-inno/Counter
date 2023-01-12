272a3246c5a751b55d46f749542a2b125b030bed
Kunal-inno <kunalgupta@innotechteam.in>
2023-01-12 11:41:09 +0530

    start and stop of counter on click

--- src/CounterDisplay.js
+++ src/CounterDisplay.js
@@ -1,60 +1,33 @@
-// import React, { useEffect, useState } from "react";
-// import "./CounterDisplay.css";
-
-// const CounterDisplay = () => {
-//   const [counter, setCounter] = useState(0);
-
-//   useEffect(() => {
-//     const id = setInterval(() => setCounter((oldCount) => oldCount + 1), 1000);
-
-//     return () => {
-//       clearInterval(id);
-//     };
-//   }, []);
-//   return (
-//     <div>
-//       <div className="card-parent">
-//         <p className="start-stop">Counter</p>
-
-//         <p className="sec">{counter}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CounterDisplay;
 import React, { useEffect, useState } from "react";
 import "./CounterDisplay.css";
 
 const CounterDisplay = () => {
   const [counter, setCounter] = useState(0);
-  const [isCounterRunning, setIsCounterRunning] = useState(true);
-  const [intervalId, setIntervalId] = useState(null);
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
-    if (isCounterRunning) {
-      setIntervalId(setInterval(() => setCounter((oldCount) => oldCount + 1), 1000));
-    } else {
-      clearInterval(intervalId);
+    let id;
+    if (isRunning) {
+      id = setInterval(() => setCounter((oldCount) => oldCount + 1), 1000);
     }
-
     return () => {
-      clearInterval(intervalId);
+      clearInterval(id);
     };
-  }, [isCounterRunning, intervalId]);
+  }, [isRunning]);
 
-  const handleCounterClick = () => {
-    setIsCounterRunning(!isCounterRunning);
+  const handleStartStop = () => {
+    setIsRunning(!isRunning);
   };
 
   return (
     <div>
       <div className="card-parent">
-        <p className="start-stop" onClick={handleCounterClick}>Counter</p>
+        <p className="start-stop" onClick={handleStartStop}>Click to Start/Stop</p>
         <p className="sec">{counter}</p>
       </div>
     </div>
   );
 };
+
 
 export default CounterDisplay;
